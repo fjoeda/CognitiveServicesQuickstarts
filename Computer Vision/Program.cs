12134773de795796f60205a62352c552007881d9
fjoeda <f.yudanto@gmail.com>
2019-10-08 10:52:11 +0700

    Add project files.

---
+++ Computer Vision/Program.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Computer_Vision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string imageUrl = "http://i.dailymail.co.uk/i/pix/2017/04/05/08/3EF0483400000578-4379890-NOT_FOR_USE_ON_GREETING_CARDS_POSTCARDS_CALENDARS_OR_ANY_MERCHAN-a-79_1491379088606.jpg";
+            var client = Authenticate("https://botvisionku.cognitiveservices.azure.com/", "7b81e60329154bb69a90f307b238690d");
+            AnalyzeImageUrl(client, imageUrl);
+            Console.ReadLine();
+        }
+
+        public static ComputerVisionClient Authenticate(string endpoint, string key)
+        {
+            ComputerVisionClient client =
+                new ComputerVisionClient(new ApiKeyServiceClientCredentials(key))
+                { Endpoint = endpoint };
+            return client;
+        }
+
+
+        public static void AnalyzeImageUrl(ComputerVisionClient client, string imageUrl)
+        {
+            Console.WriteLine("----------------------------------------------------------");
+            Console.WriteLine("ANALYZE IMAGE - URL");
+            Console.WriteLine();
+
+            List<VisualFeatureTypes> features = new List<VisualFeatureTypes>()
+                {
+                    VisualFeatureTypes.Categories, VisualFeatureTypes.Description,
+                    VisualFeatureTypes.Faces, VisualFeatureTypes.ImageType,
+                    VisualFeatureTypes.Tags, VisualFeatureTypes.Adult,
+                    VisualFeatureTypes.Color, VisualFeatureTypes.Brands,
+                    VisualFeatureTypes.Objects
+                };
+
+            ImageAnalysis results = client.AnalyzeImageAsync(imageUrl, features).Result;
+
+            // Menampilkan Ringkasan Gambar
+            Console.WriteLine("Summary:");
+            foreach (var caption in results.Description.Captions)
+            {
+                Console.WriteLine($"{caption.Text} with confidence {caption.Confidence}");
+            }
+            Console.WriteLine();
+
+            // Menampilkan Kategori Gambar
+            Console.WriteLine("Categories:");
+            foreach (var category in results.Categories)
+            {
+                Console.WriteLine($"{category.Name} with confidence {category.Score}");
+            }
+            Console.WriteLine();
+
+            // Menampilkan Tag dari gambar
+            Console.WriteLine("Tags:");
+            foreach (var tag in results.Tags)
+            {
+                Console.WriteLine($"{tag.Name} {tag.Confidence}");
+            }
+            Console.WriteLine();
+
+        }
+    }
+}
---
+++ Face/Program.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Azure.CognitiveServices.Vision.Face;
+using Microsoft.Azure.CognitiveServices.Vision.Face.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Face
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string imageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/2/28/Child%27s_Angry_Face.jpg/1200px-Child%27s_Angry_Face.jpg";
+            var client = Authenticate("https://modulface.cognitiveservices.azure.com/", "092dc72500c545bbb568376cb19fbb17");
+            DetectFaceExtract(client, imageUrl, RecognitionModel.Recognition01);
+            Console.ReadLine();
+        }
+
+        public static IFaceClient Authenticate(string endpoint, string key)
+        {
+            return new FaceClient(new ApiKeyServiceClientCredentials(key)) { Endpoint = endpoint };
+        }
+
+        public static void DetectFaceExtract(IFaceClient client, string url, string recognitionModel)
+        {
+            Console.WriteLine("========Deteksi Wajah========");
+
+            IList<DetectedFace> detectedFaces;
+
+            // Detect faces with all attributes from image url.
+            detectedFaces = client.Face.DetectWithUrlAsync($"{url}",
+                    returnFaceAttributes: new List<FaceAttributeType> { FaceAttributeType.Accessories, FaceAttributeType.Age,
+                FaceAttributeType.Blur, FaceAttributeType.Emotion, FaceAttributeType.Exposure, FaceAttributeType.FacialHair,
+                FaceAttributeType.Gender, FaceAttributeType.Glasses, FaceAttributeType.Hair, FaceAttributeType.HeadPose,
+                FaceAttributeType.Makeup, FaceAttributeType.Noise, FaceAttributeType.Occlusion, FaceAttributeType.Smile },
+                    recognitionModel: recognitionModel).Result;
+
+            Console.WriteLine($"{detectedFaces.Count} Wajah terdeteksi.");
+            foreach (var face in detectedFaces)
+            {
+                Console.WriteLine($"Detail Wajah ID :{face.FaceId}");
+                Console.WriteLine($"Ekspresi Senang {face.FaceAttributes.Emotion.Happiness}");
+                Console.WriteLine($"Ekspresi Sedih {face.FaceAttributes.Emotion.Sadness}");
+                Console.WriteLine($"Ekspresi Marah {face.FaceAttributes.Emotion.Anger}");
+                Console.WriteLine($"Ekspresi Jijik {face.FaceAttributes.Emotion.Disgust}");
+                Console.WriteLine($"Ekspresi Netral {face.FaceAttributes.Emotion.Neutral}");
+                Console.WriteLine($"Ekspresi Takut {face.FaceAttributes.Emotion.Fear}");
+                Console.WriteLine($"Umur {face.FaceAttributes.Age}");
+                Console.WriteLine($"Gender {face.FaceAttributes.Gender}");
+            }
+            
+        }
+    }
+}
